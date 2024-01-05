822e77aed6403ac6a0814d3ca6e48e051bca9951
SeemaBehera <beheraseema387@gmail.com>
2024-01-05 06:20:24 +0530

    1st commit

---
+++ src/main/java/comm/app/demo/JenkinsProject/App.java
@@ -0,0 +1,13 @@
+package comm.app.demo.JenkinsProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
